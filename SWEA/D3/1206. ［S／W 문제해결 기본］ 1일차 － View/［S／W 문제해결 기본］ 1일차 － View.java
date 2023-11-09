d62b8a4cb0c1d4a7c204fa71c649432cc4ab99ff
HyeinYang <72717195+happynow7@users.noreply.github.com>
2023-11-09 21:54:23 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 277 ms, Memory: 51,484 KB -BaekjoonHub

---
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.FileInputStream;
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+     
+        Scanner sc = new Scanner(System.in);
+        int widthMax = 1000;
+        int heightMax = 255;
+        int test_count = 10;
+ 
+        for(int test_case = 1; test_case <= test_count; test_case++)
+        {
+            int buildingCount = sc.nextInt();
+ 
+            int[][] map = new int[widthMax][heightMax];
+ 
+            // 빌딩 정보 입력
+            for(int width=0; width<buildingCount; width++){
+                int heightLimit = sc.nextInt();
+                for(int height=0; height<heightLimit; height++){
+                    map[width][height] = 1;
+                }
+            }
+ 
+            //1번째 빌딩 1층부터 왼쪽 오른쪽 확인
+            int count = 0;
+            for(int width=(0+2); width<(widthMax-2); width++){   //맨 왼쪽 오른쪽 두칸은 건물 지어지지 않음
+ 
+                for(int height=0; height<heightMax; height++){
+                    boolean left = (map[width-1][height] == 0) && (map[width-2][height] == 0);
+                    boolean right = (map[width+1][height] == 0) && (map[width+2][height] == 0);
+                    if(map[width][height] == 1 && left && right ){
+                        count++;
+                    }
+                }
+            }
+            //System.out.println(Arrays.deepToString(map));
+ 
+            System.out.println("#" + test_case + " " + count);
+        }
+         
+    }
+}
