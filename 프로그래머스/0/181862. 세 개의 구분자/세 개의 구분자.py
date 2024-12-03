188e043f99a53db8b73218cd2bb5da172d93841f
HyeinYang <72717195+happynow7@users.noreply.github.com>
2024-12-03 11:19:52 +0900

    [level 0] Title: 세 개의 구분자, Time: 9.32 ms, Memory: 23.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
@@ -0,0 +1,11 @@
+def solution(myStr):
+    answer = []
+    
+    for i in ['a', 'b', 'c']:
+        myStr = myStr.replace(i, ' ')
+    answer = myStr.split()
+    
+    if not answer:
+        answer = ["EMPTY"]
+    
+    return answer
