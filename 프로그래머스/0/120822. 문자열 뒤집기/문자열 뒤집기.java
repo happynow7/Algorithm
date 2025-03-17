a2d46fbe0fb9bc2456d5cea5b7b295b1b545ca38
HyeinYang <72717195+happynow7@users.noreply.github.com>
2025-03-17 18:12:44 +0900

    [level 0] Title: 문자열 뒤집기, Time: 13.20 ms, Memory: 93.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        for (int i = my_string.length()-1; i>=0; i--){
+            answer += my_string.charAt(i);
+        }
+        return answer;
+    }
+}
+
+
+// class Solution {
+//     public String solution(String my_string) {
+//         StringBuilder answer = new StringBuilder();
+        
+//         for (int i = my_string.length() - 1; i >= 0; i--) {
+//             answer.append(my_string.charAt(i));
+//         }
+//         return answer.toString();
+//     }
+// }
