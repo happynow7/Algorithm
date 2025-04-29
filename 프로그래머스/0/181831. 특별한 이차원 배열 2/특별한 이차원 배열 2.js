77c110e5bfd4469ced745fe0042d11c277931460
HyeinYang <72717195+happynow7@users.noreply.github.com>
2025-04-29 22:42:08 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 22.78 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+    const len = arr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            if (arr[i][j] !== arr[j][i]) {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
