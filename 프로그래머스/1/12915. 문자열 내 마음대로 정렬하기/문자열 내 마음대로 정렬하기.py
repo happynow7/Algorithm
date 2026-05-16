7158f05ce4ea008dc7f54dc96f3d78147159a998
HyeinYang <72717195+happynow7@users.noreply.github.com>
2026-05-16 20:59:18 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.02 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
@@ -1,5 +1,5 @@
 def solution(strings, n):
-    answer = []
-    answer = sorted(strings, key=lambda x:(x[n],x))
-
+    answer = sorted(strings, key = lambda x : (x[n], x))
+    
+    
     return answer
